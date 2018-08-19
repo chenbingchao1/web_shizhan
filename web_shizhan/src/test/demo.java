57f0260cd264065ef0f3394f62a810c319848b98
ccc <ccc@163.com>
2018-08-19 10:27:40 +0800

    11111-忽略class文件提交，推送后其他人应该不会有class冲突

--- web_shizhan/src/test/demo.java
+++ web_shizhan/src/test/demo.java
@@ -34,4 +34,9 @@ public class demo {
 		List<Category> list = service.getCategory();
 		System.out.println("真正的实战项目");
 	}
+	@Test
+	public void getOne3() {
+		List<Category> list = service.getCategory();
+		System.out.println("1111再次提交，此时已经忽略class文件，推送后，class文件应该不会冲突");
+	}
 }
